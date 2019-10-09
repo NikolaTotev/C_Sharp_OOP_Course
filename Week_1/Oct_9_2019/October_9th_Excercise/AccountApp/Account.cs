336c1fd7e4b7cfa54be25703f660f1d85050ba6b
Nikola <nikola.r.totev@gmail.com>
2019-10-09 16:27:24 +0300

    Oct_9_2019_Fisrt_Hour_Backup

---
+++ Week_1/Oct_9_2019/October_9th_Excercise/AccountApp/Account.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AccountApp
+{
+    /// <summary>
+    /// Class account
+    /// Purpose:
+    /// Datamembers:B
+    /// </summary>
+    public class Account
+    {
+        private decimal anualInterestRate;
+
+        private decimal balance;
+        private int dateCreated;
+        private int id;
+
+        public int AnualInterestRate
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int Balance
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int DateCreated
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int Id
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public void Deposit()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Withdraw()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
