82869f4e6f4d95b224aedaf67a424e2d4fd65eb7
Nikola <nikola.r.totev@gmail.com>
2019-10-10 16:39:59 +0300

    Added invoice class

---
+++ Week_1/Oct_9_2019/October_9th_Excercise/Invoice/Invoice.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Invoice
+{
+    public class Invoice
+    {
+        private int partDescription;
+        private int partNumber;
+        private int pricePerItem;
+        private int quantity;
+
+        public int PartDescription
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int PartNumber
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int PricePerItem
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int Quantity
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public void GetInvoiceAmount()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void GetInvoice()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
