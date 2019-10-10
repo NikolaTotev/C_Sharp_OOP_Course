9c159491d402222a7182a27cca8a2400d7bc0da9
Nikola <nikola.r.totev@gmail.com>
2019-10-10 16:21:45 +0300

    Finisehd creating time class, modified gradebook class

---
+++ Week_1/Oct_9_2019/October_9th_Excercise/AccountApp/Time.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AccountApp
+{
+    public class Time
+    {
+        private int hour;
+        private int minute;
+        private int second;
+
+        public Time(int hour)
+        {
+            Hour = hour;
+        }
+
+        public int Hour
+        {
+            get => default;
+            set
+            {
+                if (value != null)
+                {
+                    hour = value;
+                }
+            }
+        }
+
+        public int Minute
+        {
+            get => default;
+            set
+            {
+                if (value != null)
+                {
+                    minute = value;
+                }
+            }
+        }
+
+        public int Second
+        {
+            get => default;
+            set
+            {
+                if (value != null)
+                {
+                   second = value;
+                }
+            }
+        }
+    }
+}
