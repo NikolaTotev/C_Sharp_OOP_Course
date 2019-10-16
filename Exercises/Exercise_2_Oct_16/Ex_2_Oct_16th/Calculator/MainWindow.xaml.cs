fa9305a9d009473e950de6c9934aea153c6d469a
Nikola <nikola.r.totev@gmail.com>
2019-10-16 16:58:57 +0300

    Added functionality to calculator

--- Exercises/Exercise_2_Oct_16/Ex_2_Oct_16th/Calculator/MainWindow.xaml.cs
+++ Exercises/Exercise_2_Oct_16/Ex_2_Oct_16th/Calculator/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +21,137 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
+            currentOperation = Operations.NO_OP;
         }
+
+        #region Data Members
+        private double leftInput;
+        private double result;
+
+        private Operations currentOperation;
+        private enum Operations
+        {
+            ADD, SUBTR, MULTIP, DIVIDE, NO_OP
+        }
+
+        #endregion
+
+        #region Operation event handlers
+
+        private void OperationProcessing(string operationText)
+        {
+            switch (operationText)
+            {
+                case "+":
+                    currentOperation = Operations.ADD;
+                    break;
+
+                case "-":
+                    currentOperation = Operations.SUBTR;
+                    break;
+
+                case "X":
+                    currentOperation = Operations.MULTIP;
+                    break;
+
+                case "/":
+                    currentOperation = Operations.DIVIDE;
+
+                    break;
+                default:
+                    currentOperation = Operations.NO_OP;
+                    break;
+            }
+
+            leftInput = Convert.ToDouble(TxtInput.Text);
+            TxtInput.Text = "0";
+
+        }
+
+        private void Btn_OperationClick(object sender, RoutedEventArgs e)
+        {
+            OperationProcessing((string)((Button)sender).Content);
+        }
+
+        private void Btn_ComputeClick(object sender, RoutedEventArgs e)
+        {
+            switch (currentOperation)
+            {
+                case Operations.ADD:
+                    result = leftInput + Convert.ToDouble(TxtInput.Text);
+                    break;
+                case Operations.SUBTR:
+                    result = leftInput - Convert.ToDouble(TxtInput.Text);
+                    break;
+                case Operations.MULTIP:
+                    result = leftInput * Convert.ToDouble(TxtInput.Text);
+                    break;
+                case Operations.DIVIDE:
+                    double rightInput = Convert.ToDouble(TxtInput.Text);
+                    if (!rightInput.Equals(0))
+                    {
+                        result = leftInput / Convert.ToDouble(TxtInput.Text);
+                    }
+                    else
+                    {
+                        TxtInput.Text = "Division by 0 is undefined";
+                    }
+                    break;
+                case Operations.NO_OP:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            TxtInput.Text = result.ToString();
+            currentOperation = Operations.NO_OP;
+            result = 0;
+        }
+        #endregion
+
+        #region Number event handlers
+
+        private void TypeDigit(string digit)
+        {
+           
+            if (TxtInput.Text.Equals("0"))
+            {
+                TxtInput.Text = digit;
+            }
+            else
+            {
+
+                TxtInput.Text += digit;
+            }
+        }
+
+        private void NumberBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            TypeDigit((string)((Button)sender).Content);
+        }
+        #endregion
+
+        private void Btn_DotClick(object sender, RoutedEventArgs e)
+        {
+            TypeDigit((string)((Button)sender).Content);
+        }
+
+        #region Clearing handlers
+        private void Btn_ClearLast(object sender, RoutedEventArgs e)
+        {
+            TxtInput.Text = "0";
+        }
+
+        private void Btn_ClearAll(object sender, RoutedEventArgs e)
+        {
+            TxtInput.Text = "0";
+            currentOperation = Operations.NO_OP;
+            result = 0;
+        } 
+        #endregion
     }
 }
