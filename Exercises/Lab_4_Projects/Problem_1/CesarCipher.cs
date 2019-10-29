fa116fdd354f75b780521ba2d53e807d916e12c5
Nikola <nikola.r.totev@gmail.com>
2019-10-29 12:21:43 +0200

    Added lab 4, project 1

---
+++ Exercises/Lab_4_Projects/Problem_1/CesarCipher.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading;
+using System.Xml;
+using static System.Char;
+
+namespace Problem_1
+{
+    class CesarCipher
+    {
+        private string input;
+        private string output;
+        private int offset;
+
+        private List<string> alphabet = new List<string>()
+        {
+            "A", "B", "C", "D", "E", "F", "G", "H",
+            "I", "J", "K", "L", "M", "N", "O",
+            "P","Q","R","S","T","U","V","W",
+            "X","Y","Z",
+        };
+
+
+        public CesarCipher()
+        {
+            Console.WriteLine("Functions available are: \n Encrypt(input String, input offset) " +
+                              "\n Decrypt(input string, input offset)");
+        }
+
+        public string Encrypt(string input, int offset)
+        {
+            string output = "";
+            int letterIndex;
+            int newIndex;
+            bool wasLower = false;
+            foreach (char letter in input)
+            {
+                if (IsLower(letter))
+                {
+                    wasLower = true;
+                }
+                letterIndex = alphabet.IndexOf(letter.ToString().ToUpper());
+                if (letterIndex + offset >= alphabet.Count)
+                {
+                    newIndex = 0 + ((letterIndex + offset) - alphabet.Count);
+                }
+                else
+                {
+                    newIndex = letterIndex + offset;
+                }
+                output += (wasLower) ? alphabet[newIndex].ToLower() : alphabet[newIndex];
+                wasLower = false;
+            }
+
+            return output;
+        }
+    }
+}
---
+++ Exercises/Lab_4_Projects/Problem_1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Problem_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CesarCipher cs = new CesarCipher();
+            Console.WriteLine(cs.Encrypt("AaBbZz", 1));
+        }
+    }
+}
