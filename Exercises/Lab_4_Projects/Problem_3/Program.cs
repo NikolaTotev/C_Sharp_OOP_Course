5c331edf06f9bd3db58dcd6aab292d970e314a62
Nikola <nikola.r.totev@gmail.com>
2019-10-29 13:04:43 +0200

    Lab 4, problem  3 completed.

---
+++ Exercises/Lab_4_Projects/Problem_3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Problem_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ZipCodeConverter converter = new ZipCodeConverter();
+            Console.WriteLine(converter.GenerateBarcode(111));
+        }
+    }
+}
---
+++ Exercises/Lab_4_Projects/Problem_3/ZipCodeConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problem_3
+{
+    public class ZipCodeConverter
+    {
+        private List<string> barcodeInfo = new List<string>()
+        {
+            "||:::", //0
+            ":::||", //1
+            "::|:|", //2
+            "::||:", //3
+            ":|::|", //4
+            ":|:|:", //5 
+            ":||::", //6
+            "|:::|", //7
+            "|::|:", //8
+            "|:|::", //9
+        };
+
+        public string GenerateBarcode(int input)
+        {
+            string output = "";
+            string inputString = input.ToString();
+            int currentDigit;
+            foreach (char digit in inputString)
+            {
+                currentDigit = Convert.ToInt32(digit) - '0';
+                output += barcodeInfo[currentDigit];
+            }
+
+            return output;
+        }
+    }
+}
