3f8191484426c3ba1a27e04950970580e48da7af
Nikola <nikola.r.totev@gmail.com>
2019-10-30 16:15:40 +0200

    Added Oct 30 exercise - Learning Linq

---
+++ Exercises/Exercise_4_Oct_30_2019/Project_1/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Project_1
+{
+    class Program
+    {
+
+        public static void Display<T>(IEnumerable<T> query, string header)
+        {
+            Console.WriteLine("LINQ execution:{0}", header);
+            foreach (var item in query)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+        }
+        public static void Main(string[] args)
+        {
+            Invoice[] invoices =
+            {
+                new Invoice(83, "Electric Sander", 7, 57.98m),
+                new Invoice(24, "Power saw", 18, 99.99m),
+                new Invoice(7, "Sledge hammer", 11, 21.50m),
+                new Invoice(77, "Hammer", 76, 11.99m),
+                new Invoice(77, "Hammer", 30, 11.99m),
+                new Invoice(39, "Lawn mower", 3, 79.50m),
+                new Invoice(39, "Lawn mower", 6, 79.50m),
+                new Invoice(68, "Screwdriver", 106, 6.99m),
+                new Invoice(56, "Jig saw", 21, 11.00m),
+                new Invoice(3, "Wrench", 34, 7.50m)
+            };
+
+            var sortedByDesc = from invoice in invoices
+                               orderby invoice.PartDescription
+                               select invoice;
+
+            Display(sortedByDesc, "Sorted by description.");
+
+
+            var sortedByQuantity = from invoice in invoices
+                                   orderby invoice.Quantity
+                                   select invoice;
+
+            Display(sortedByQuantity, "Sorted by quantity.");
+
+
+            var sortedByQuantity2 = from invoice in invoices
+                                    orderby invoice.PartNumber descending
+                                    select new { Description = invoice.PartDescription, Quantity = invoice.Quantity };
+
+            foreach (var item in sortedByQuantity2)
+            {
+                Console.WriteLine("{0, 20}{1 ,5}", item.Description, item.Quantity);
+            }
+            Console.WriteLine();
+
+
+            var sortedByQuantityWithDoubles = from invoice in invoices //this is what var is IEnumerable<(string, int)>
+                                              orderby invoice.PartNumber descending
+                                              select (invoice.PartDescription, invoice.Quantity); ///Here you can return a couple of things at once.
+
+
+
+            foreach (var item in sortedByQuantityWithDoubles)
+            {
+                Console.WriteLine("{0, 20}{1 ,5}", item.PartDescription, item.Quantity);
+            }
+            Console.WriteLine();
+
+            foreach ((string, int) item in sortedByQuantityWithDoubles)
+            {
+                Console.WriteLine("{0, 20}{1 ,5}", item.Item1, item.Item2);
+            }
+            Console.WriteLine();
+
+
+            var sortedInRange = from invoice in invoices
+                                let total = invoice.Quantity * invoice.Price
+                                orderby total where total > 200 && total < 500
+                                select (invoice.PartDescription, total);
+
+            Display(sortedInRange, "Totals where total between 200 and 500");
+
+        }
+    }
+}
---
+++ Exercises/Lab_4_Projects/Problem_7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Problem_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static int[][] MultiplyMatricies(int[][] matrix1, int[][] matrix2)
+        {
+            int size1 = matrix1.Length;
+            int[][] result;
+
+        }
+    }
+}
