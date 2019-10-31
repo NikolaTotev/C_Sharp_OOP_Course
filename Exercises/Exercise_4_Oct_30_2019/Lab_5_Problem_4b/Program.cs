380d6784110448edf0a75c6aa493071dd38d3f5c
Nikola <nikola.r.totev@gmail.com>
2019-10-31 16:20:20 +0200

    Completed Lab 5 Problem 4b

---
+++ Exercises/Exercise_4_Oct_30_2019/Lab_5_Problem_4b/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_5_Problem_4b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Given list of strings 
+            List<string> words = new List<string>() { "Blueberry", "Chimpanzee", "Abacus", "Banana", "Apple", "Cheese" };
+
+            //Grouping strings by their first letter
+            var groupedByFirstLetter = from word in words
+                                      group word by word[0] into grp
+                                      select grp;
+
+            //Printing out all the grouped elements.
+            foreach (var group in groupedByFirstLetter)
+            {
+                Console.WriteLine("Group that starts with {0} has {1} elements", group.Key, group.Count());
+                foreach (var element in group)
+                {
+                    Console.WriteLine(element);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        
+    }
+}
