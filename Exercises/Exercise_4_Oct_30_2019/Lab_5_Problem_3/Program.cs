b9c45049312d54f2caa578414b33448e084fc48b
Nikola <nikola.r.totev@gmail.com>
2019-10-31 15:54:39 +0200

    Completed problem 3 from lab 5

--- Exercises/Exercise_4_Oct_30_2019/Lab_5_Problem_3/Program.cs
+++ Exercises/Exercise_4_Oct_30_2019/Lab_5_Problem_3/Program.cs
@@ -22,24 +22,60 @@ namespace Lab_5_Problem_3
             Console.WriteLine("Unsorted chars");
             Console.WriteLine("{0}", string.Join(", ", chars));
             Console.WriteLine();
+            //==================================
 
 
-            Console.WriteLine("Sorted chars");
+            //Sorting chars in ascending order;
+            Console.WriteLine("Sorted chars - Ascending Order");
 
-            var sortedChars = from chr in chars
-                orderby chr
+            var sortedAscending = from chr in chars
+                orderby chr  ascending 
                 select chr;
 
-            foreach (var chr in sortedChars)
+            foreach (var chr in sortedAscending)
             {
                 Console.Write("{0}, ", chr);
             }
 
             Console.WriteLine();
-
-            Console.WriteLine("Sorted chars with lambda");
-            
             Console.WriteLine();
+            //==================================
+
+
+            //Sorting chars in descending order;
+            Console.WriteLine("Sorted chars - Descending Order");
+
+            var sortedDescending = from chr in chars
+                orderby chr descending 
+                select chr;
+
+            foreach (var chr in sortedDescending)
+            {
+                Console.Write("{0}, ", chr);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+            //==================================
+
+
+            //Chars with removed duplicates
+            Console.WriteLine("Sorted chars - Duplicates removed");
+
+            var sortdWithDuplicatesRemoved = from chr in chars
+                orderby chr descending
+                select chr;
+
+            //var distinctItems = sortdWithDuplicatesRemoved.Select(y => y.First());
+
+            List<char> distinctElements = sortdWithDuplicatesRemoved.Distinct().ToList();
+            foreach (char element in distinctElements)
+            {
+                Console.Write("{0}, ", element);
+            }
+
+            Console.WriteLine();
+            //==================================
 
         }
     }
